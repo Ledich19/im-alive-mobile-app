4f4aa600d5ae5281b24d320eae7f768765d91dfc
Artem <artemko77788@gmail.com>
2023-10-30 18:15:25 +0200

    add reset password

--- src/screens/auth/ForgotPasswordScreen.tsx
+++ src/screens/auth/ForgotPasswordScreen.tsx
@@ -1,17 +1,104 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { sendPasswordResetEmail } from 'firebase/auth';
+
+import AuthInput from '../../components/AuthInput';
+import MainButton from '../../components/buttons/MainButton';
+import SmallButton from '../../components/buttons/SmallButton';
+import { Text, View as ThemeView } from '../../components/Themed';
+import { auth } from '../../config/firebase';
+
+const initialState = {
+  email: '',
+};
+
+interface IState {
+  email: string;
+}
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'green',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 32,
+  },
+
+  title: {
+    alignSelf: 'center',
+    color: '#2A3547',
+    fontSize: 20,
+    marginBottom: 24,
+  },
+  bottom: { marginBottom: 24 },
+  bottomRedirectWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
-const ForgotPasswordScreen = () => {
+interface ILoginScreen {
+  navigation: StackNavigationProp<any>;
+}
+
+const ForgotPasswordScreen: React.FC<ILoginScreen> = ({ navigation }) => {
+  const [state, setState] = useState<IState>(initialState);
+
+  const handleReset = async () => {
+    try {
+      await sendPasswordResetEmail(auth, state.email);
+    } catch (error) {
+      console.log('error :>> ', error);
+    }
+  };
+
+  const hideKeyboardOnTouch = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View>
-      <Text style={styles.text}>ForgotPasswordScreen</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={hideKeyboardOnTouch} style={{ backgroundColor: 'green' }}>
+      <ThemeView style={styles.container}>
+        <View>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <View>
+              <Text style={styles.title}>Reset pasword</Text>
+            </View>
+            <View>
+              <View style={styles.bottom}>
+                <AuthInput
+                  placeholder="Email"
+                  state={state.email}
+                  onChangeText={(text) => {
+                    setState((prevState) => ({ ...prevState, email: text }));
+                  }}
+                />
+              </View>
+
+              <View style={styles.bottom}>
+                <MainButton title="Change password" handlePress={handleReset} />
+              </View>
+
+              <View style={styles.bottomRedirectWrapper}>
+                <Text>You don`t have an account? </Text>
+                <SmallButton
+                  title="To register"
+                  handlePress={() => navigation.navigate('RegistrationScreen')}
+                />
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </View>
+      </ThemeView>
+    </TouchableWithoutFeedback>
   );
 };
 
