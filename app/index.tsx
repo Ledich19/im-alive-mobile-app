64321f047fb22957b994be9af6478e7c1bf32dad
Aleksandr <ledich19@gmail.com>
2023-11-10 10:19:29 +0200

    fix:

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,9 @@ import FlashMessage from 'react-native-flash-message';
 import { SettingsProvider } from './context';
 import Navigation from '../src/config/Navigation';
 
-// if (__DEV__) {
-//   import('../ReactotronConfig.js').then(() => console.log('Reactotron Configured'));
-// }
+if (__DEV__) {
+  import('../ReactotronConfig.js').then(() => console.log('Reactotron Configured'));
+}
 
 export default function App() {
   return (
